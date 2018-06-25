cc605b4fb5397db9f4262c16531bbf01471e39ff
Karl Mark Balagtey <karl.balagtey@gmail.com>
2018-06-26 00:21:08 +0100

    Initial icon and splash scree, map view load

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import { MapView } from 'expo';
 
 class MapScreen extends Component {
+	state = {
+		region: {
+			longitude: -122,
+			latitude: 37,
+			longitudeDelta: 0.04,
+			latitudeDelta: 0.09
+		}
+	}
+
 	render() {
 		return (
-			<View>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
-				<Text>MapScreen</Text>
+			<View style={{ flex: 1 }}>
+				<MapView 
+					region={this.state.region}
+					style={{ flex: 1 }}
+				/>
 			</View>
 		);
 	}
